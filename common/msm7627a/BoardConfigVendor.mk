a65bc92f1aceba17ae54705c07da5481aae01761
alexforsale <alexforsale@yahoo.com>
2014-05-03 14:10:27 +0700

    EGL 1.4 QUALCOMM build: AU_LINUX_ANDROID_ICS_STRAWBERRY.04.00.04.19.084_msm7627a_ICS_STRAWBERRY_CL2558087_release_AU (CL2558087)

--- common/msm7627a/BoardConfigVendor.mk
+++ common/msm7627a/BoardConfigVendor.mk
@@ -25,3 +25,25 @@ PRODUCT_PACKAGES += \
     softmac \
     utf.bin
 endif
+
+# adreno files
+PRODUCT_PACKAGES += \
+    eglsubAndroid \
+    libEGL_adreno200 \
+    libGLESv1_CM_adreno200 \
+    libGLESv2_adreno200 \
+    libq3dtools_adreno200 \
+    a225p5_pm4.fw \
+    a225_pfp.fw \
+    a225_pm4.fw \
+    a300_pfp.fw \
+    a300_pm4.fw \
+    leia_pfp_470.fw \
+    leia_pm4_470.fw \
+    yamato_pfp.fw \
+    yamato_pm4.fw \
+    libC2D2 \
+    libgsl \
+    libOpenVG \
+    libsc-a2xx
+
